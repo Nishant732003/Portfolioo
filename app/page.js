8e99d8679b11aebc2040e293b87b96e45f3f35bb
nishant <mahtonishantraj@gmail.com>
2024-07-05 01:31:21 +0530

    trying to deploy

--- app/page.js
+++ app/page.js
@@ -1,10 +1,10 @@
 
-import AboutSection from "@components/homepage/about";
-import ContactSection from "@components/homepage/contact";
-import Education from "@components/homepage/education";
-import HeroSection from "@components/homepage/hero-section";
-import Skills from "@components/homepage/skills";
-import ProjectCards from "@components/homepage/projects";
+import AboutSection from "@components/homepage/about/index";
+import ContactSection from "@components/homepage/contact/index";
+import Education from "@components/homepage/education/index";
+import HeroSection from "@components/homepage/hero-section/index";
+import Skills from "@components/homepage/skills/index";
+import ProjectCards from "@components/homepage/projects/index";
 
 export default async function Home() {
   return (
