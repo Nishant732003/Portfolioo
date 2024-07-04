71a06baf8f023792876207440f8ecbfaa68f28b0
nishant <mahtonishantraj@gmail.com>
2024-07-04 22:09:19 +0530

    updated imports

--- app/page.js
+++ app/page.js
@@ -1,13 +1,13 @@
 // import { personalData } from "@/utils/data/personal-data";
 "use strict"
-import AboutSection from "../../../Portfolio/portfolio/app/components/homepage/about";
+import AboutSection from "../../../Portfolio/portfolio/app/components/homepage/about/index";
 
-import ContactSection from "../../../Portfolio/portfolio/app/components/homepage/contact";
-import Education from "../../../Portfolio/portfolio/app/components/homepage/education";
+import ContactSection from "../../../Portfolio/portfolio/app/components/homepage/contact/index";
+import Education from "../../../Portfolio/portfolio/app/components/homepage/education/index";
 // import Experience from "./components/homepage/experience";
-import HeroSection from "../../../Portfolio/portfolio/app/components/homepage/hero-section";
+import HeroSection from "../../../Portfolio/portfolio/app/components/homepage/hero-section/index";
 // import Projects from "./components/homepage/projects";
-import Skills from "../../../Portfolio/portfolio/app/components/homepage/skills";
+import Skills from "../../../Portfolio/portfolio/app/components/homepage/skills/index";
 import ProjectCards from "./components/homePage/projects/index";
 
 export default async function Home() {
