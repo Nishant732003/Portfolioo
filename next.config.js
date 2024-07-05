7f2e9203c374eaea5750a8b4f13c3e9ac4ed7379
nishant <mahtonishantraj@gmail.com>
2024-07-06 02:48:06 +0530

    change

--- next.config.js
+++ next.config.js
@@ -5,20 +5,9 @@ import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+export default {
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
   },
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      "@components": path.join(__dirname, "components"),
-      "@utils": path.join(__dirname, "utils"),
-    };
-    
-    return config;
-  },
+  
 };
-
-export default nextConfig;
